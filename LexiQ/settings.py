35223d4342146a57a5632fdf9f3efad6d5642de7
silentle <1917400306@qq.com>
2024-03-09 19:11:47 +0800

    准备部署系统

--- LexiQ/settings.py
+++ LexiQ/settings.py
@@ -127,4 +127,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR,"files" ,'static')
