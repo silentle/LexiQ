019ecc10abb9f95b01ffd302a87c8acea2225e8a
silentle <89768772+silentle@users.noreply.github.com>
2024-04-12 16:03:42 +0800

    修改import

--- wordapp/tests.py
+++ wordapp/tests.py
@@ -1,8 +1,10 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
-from .models import Word, WordGroup, StudyRecord
+from .models import Word, WordGroup, StudyRecord,UserAchievement
+from .views import add_achievements
 from django.utils import timezone
+from datetime import timedelta
 
 
 class WordModelTest(TestCase):
@@ -78,3 +80,34 @@ class ViewTests(TestCase):
         response = self.client.get(reverse('game', kwargs={'group_id': 1}))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'wordapp/game.html')
+        
+class StudyAchievementTest(TestCase):
+    @classmethod
+    #测试成就能否正常添加
+    def setUpTestData(cls):
+        group = WordGroup.objects.create(name='Test Group')
+        user = User.objects.create_user(username='testuser', password='12345')
+        userid=user.id
+        Word.objects.create(word='test', meaning='test meaning', group=group)
+        for i in range(1010):#创建测试用例
+            today = timezone.now().date()
+            word=Word.objects.create(word=i, meaning='test meaning'+str(i), group=group)
+            StudyRecord.objects.create(user=user, timestamp=today - timedelta(days=i),word_id=word.id)
+            #timestamp无法手动设置，只能自动添加，所以这是一天学1010个单词时候的情况
+        
+
+    def test_achievement(self):
+
+        user = User.objects.get(username='testuser')
+        user_id=user.id
+
+        self.user=user
+        add_achievements(user_id)
+        self.assertTrue(UserAchievement.objects.filter(user=self.user, achievement__name="千里之行").exists())
+        self.assertTrue(UserAchievement.objects.filter(user=self.user, achievement__name="拾级而上").exists())
+        self.assertTrue(UserAchievement.objects.filter(user=self.user, achievement__name="积少成多").exists())
+        self.assertTrue(UserAchievement.objects.filter(user=self.user, achievement__name="词汇大师").exists())
+        self.assertTrue(UserAchievement.objects.filter(user=self.user, achievement__name="学富五车").exists())
+        self.assertTrue(UserAchievement.objects.filter(user=self.user, achievement__name="博闻强识").exists())
+
+
