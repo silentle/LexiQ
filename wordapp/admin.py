a79993a7c3b562edb4f6557cef53bc4276fa66d3
silentle <89768772+silentle@users.noreply.github.com>
2024-04-12 15:50:57 +0800

    修改import

--- wordapp/admin.py
+++ wordapp/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Word, WordGroup, StudyRecord, Achievement
+from .models import Word, WordGroup, StudyRecord, Achievement,UserAchievement
 admin.site.register(Word)
 admin.site.register(WordGroup)
 admin.site.register(StudyRecord)
 admin.site.register(Achievement)
-
-# Register your models here.
+admin.site.register(UserAchievement)
