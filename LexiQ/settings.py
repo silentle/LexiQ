c85c0d650102d3f7d58c21b3f7058d0360b3aca4
silentle <89768772+silentle@users.noreply.github.com>
2024-04-12 17:08:54 +0800

    删除多余的静态资源文件

--- LexiQ/settings.py
+++ LexiQ/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-STATIC_ROOT = os.path.join(BASE_DIR,"files" ,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,"cache" ,'static')
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
